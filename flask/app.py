c72a73f8fad01eed6aee55dea6aaa5881e1687bf
1taroh <65810214+1taroh@users.noreply.github.com>
2023-10-12 21:15:37 +0900

    added serial communication

--- flask/app.py
+++ flask/app.py
@@ -1,7 +1,15 @@
 from flask import Flask
+import serial
 
 app = Flask(__name__)
 
+# ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1) # for linux
+ser = serial.Serial('COM3', 9600, timeout=1) # for windows 
+# mode | find "COM" # use this command to find the serial port in terminal
+
+temperature = ser.readline().decode()
+temperature = temperature.rstrip("\r\n")
+
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>temperature : {}</p>".format(temperature)
