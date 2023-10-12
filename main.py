3739c71a94ee1961c58891032db0ee008645bc40
1taroh <65810214+1taroh@users.noreply.github.com>
2023-10-12 20:26:44 +0900

    added sleep and error exception

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import serial
 import matplotlib.pyplot as plt
 from matplotlib import animation
+import time
 
 # ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1) # for linux
 ser = serial.Serial('COM3', 9600, timeout=1) # for windows 
@@ -13,25 +14,31 @@ data = []
 fig = plt.figure()
 
 while True:
-    # serial
-    temperature = ser.readline().decode()
-    temperature = temperature.rstrip("\r\n")
-    # print(data.decode())
+    try:
+        # serial
+        temperature = ser.readline().decode()
+        temperature = temperature.rstrip("\r\n")
+        # print(data.decode())
 
-    if len(data) >= max_size:
-        data.pop(0)
-    data.append(float(temperature))
-    # print("----------")
-    print(temperature)
+        if len(data) >= max_size:
+            data.pop(0)
+        data.append(float(temperature))
+        # print("----------")
+        print(temperature)
+        time.sleep(1) # seconds
 
-    # figure
+        # figure
+        # plt.plot(data)
+        # plt.draw()
+        # plt.pause(1)
+        # plt.cla()
+    #     if len(data) >= max_size:
+    #         break
     # plt.plot(data)
-    # plt.draw()
-    # plt.pause(1)
-    # plt.cla()
-#     if len(data) >= max_size:
-#         break
-# plt.plot(data)
-# plt.ylim(-10,40)
-# plt.savefig("./figure/fig.png")
+    # plt.ylim(-10,40)
+    # plt.savefig("./figure/fig.png")
+
+    except KeyboardInterrupt as e:
+        print("Keyboard Interupt")
+        break
 ser.close()
